5444370f0c7a577c753ad569af8f1ae0c284f6a1
Vishal Kumar Kesarwani <143657691+vishalkk2805@users.noreply.github.com>
2024-03-31 12:42:55 +0530

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.38.0"  # Using a minimum version constraint
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-2"  
+} 
